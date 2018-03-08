6f452e99f11c4e02b73b5781b0131da10d9ae94b
Sebastian Deisel <sebdei@users.noreply.github.com>
2018-03-08 21:25:30 +0100

    routes

---
+++ app/javascript/router/products.js
@@ -0,0 +1,27 @@
+import EditProduct from '.././components/products/EditProduct'
+import ListProduct from '.././components/products/ListProduct'
+import NewProduct from '.././components/products/NewProduct'
+import ShowProduct from '.././components/products/ShowProduct'
+
+export default [
+  {
+    path: '/products/:id/edit',
+    name: 'EditProduct',
+    component: EditProduct
+  },
+  {
+    path: '/products',
+    name: 'ListProduct',
+    component: ListProduct
+  },
+  {
+    path: '/products/new',
+    name: 'NewProdut',
+    component: NewProduct
+  },
+  {
+    path: '/products/:id',
+    name: 'ShowProduct',
+    component: ShowProduct
+  }
+]
